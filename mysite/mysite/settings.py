18c078d1c11d1f407e2e4fdabe3a6e980b72af1f
Paweł Bogaczewicz <pawel.bogaczewicz@outlook.com>
2018-08-05 01:15:00 +0200

    branch 4 preparing nerd to fm

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,6 +75,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '127.0.0.1',
+        'PORT': '5433',
         'NAME': 'd20_db1',
         'USER': 'd20',
         'PASSWORD': 'Getting started'
